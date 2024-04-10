15acf30306999d0f938ba5c14bf0ca40dd4499e1
Randy Glasgow <Glasgowrandy@gmail.com>
2024-04-09 20:35:10 -0600

    Add useAuth hook to LoginButton component

--- src/components/shared/Button/LoginButton.tsx
+++ src/components/shared/Button/LoginButton.tsx
@@ -1,7 +1,10 @@
-import { SignInButton } from '@clerk/nextjs';
+import { SignInButton, useAuth } from '@clerk/nextjs';
 import { Button } from '@components/core/Button';
 
 export const LoginButton = () => {
+  const { isSignedIn } = useAuth();
+  if (isSignedIn) return null;
+
   return (
     <SignInButton>
       <Button>Sign in</Button>
